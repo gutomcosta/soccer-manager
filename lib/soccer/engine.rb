95722484bd50be224b8442e632dbb2c6ff3ed3f7
Luiz Costa <gutomcosta@gmail.com>
2014-12-16 18:43:50 -0200

    commit inicial

---
+++ lib/soccer/engine.rb
@@ -0,0 +1,7 @@
+require "soccer/engine/version"
+
+module Soccer
+  module Engine
+    # Your code goes here...
+  end
+end
---
+++ spec/engine/engine_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Soccer::Engine do 
+
+  it 'does stuff ' do 
+    pending 
+  end
+end
